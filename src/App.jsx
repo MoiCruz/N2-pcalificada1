a4854266c11eb5166a7c4f979315a1b62cd6aa72
Moises Cruz <serviciopatrimonial@gmail.com>
2025-01-20 15:46:20 -0600

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,66 @@
-import {bill} from "./assets/images/BillMahoney.png"
-
 
 export default function App() {
   return (
     <>
-      <div className="container p-12">
-        <div className="flex flex-row">
-          <h1 className="font-extrabold font text-2xl text-center">El equipo creativo</h1>
+      <div className="p-12 flex justify-between">
+        <div className="flex">
+          <h1 className="font-extrabold font text-4xl text-center">El equipo creativo</h1>
         </div>
         <div className="">
-          <h2 className="text-start font-black">¿Quiénes somos?</h2>
+          <h2 className="text-start font-black text-2xl">¿Quiénes somos?</h2>
           <p className="text-balance">Somos un equipo de personas creativamente diversas, motivadas e innovadoras que trabajan en varios lugares del mundo.</p>
         </div>
-        <div className="flex flex-row p-12">
-          <span className="font-black h-72 w-44">DUEÑO DEL PRODUCTO</span>
-          <img src={bill} alt="" className="h-72 w-44" />
-          <p>Bill Mahoney</p>
-          <span className="font-black h-72 w-44">DIRECTORA DE ARTE</span>
-          <img src="./assets/images/SabaCabrera.png" alt="" className="h-72 w-44" />
-          <p>Saba Cabrera</p>
-          <span className="font-black h-72 w-44">DUEÑO DEL PRODUCTO</span>
-          <img src="./assets/images/ShaeLe.png" alt="" className="h-72 w-44" />
-          <p>Shae Le</p>
-        </div>
-        <div className="flex flex-row p-12">
-          <span className="font-black h-72 w-44">DISEÑADORA UX</span>
-          <img src="./assets/images/SkylahLu.png" alt="" className="h-72 w-44" />
-          <p>Skylah Lu</p>
-          <span className="font-black h-72 w-44">DESARROLLADOR</span>
-          <img src="./assets/images/GriffRichards.png" alt="" className="h-72 w-44" />
-          <p>Griff Richards</p>
-          <span className="font-black h-72 w-44">DESARROLLADOR</span>
-          <img src="./assets/images/StanJohn.png" alt="" className="h-72 w-44" />
-          <p>Stan John</p>
+      </div>
+      <div>
+        <div className="flex flex-row p-12 justify-center">
+          <div className="flex">
+            <span className="">
+              <img src="../public/photos/BillMahoney.png" alt="" className="h-80 w-52" />
+              <p className=" font-black">Bill Mahoney</p>
+            </span>
+            <h3 className=" h-5 flex rotate-90">DUEÑO DEL PRODUCTO</h3>
+          </div>
+          <div className="flex">
+            <span className="">
+              <img src="../public/photos/SabaCabrera.png" alt="" className="h-80 w-52" />
+              <p>Saba Cabrera</p>
+            </span>
+            <h3 className=" h-5 flex rotate-90">DIRECTORA DE ARTE</h3>
+          </div>
+          <div className="flex">
+            <span className="">
+              <img src="../public/photos/ShaeLe.png" alt="" className="h-80 w-52" />
+              <p>Shae Le</p>
+            </span>
+            <h3 className=" h-5 flex rotate-90">DUEÑO DEL PRODUCTO</h3>
+          </div>
 
         </div>
-      </div>
+        <div className="flex flex-row p-12 justify-center">
+          <div className="flex">
+            <span className="">
+              <img src="../public/photos/SkylahLu.png" alt="" className="h-80 w-52" />
+              <p>Skylah Lu</p>
+            </span>
+            <h3 className=" h-5 flex rotate-90">DISEÑADORA UX</h3>
+          </div>
+          <div className="flex">
+            <span className="">
+              <img src="../public/photos/GriffRichards.png" alt="" className="h-80 w-52" />
+              <p>Griff Richards</p>
+            </span>
+            <h3 className=" h-5 flex rotate-90">DESARROLLADOR</h3>
+          </div>
+          <div className="flex">
+            <span className="">
+              <img src="../public/photos/StanJohn.png" alt="" className="h-80 w-52" />
+              <p>Stan John</p>
+            </span>
+            <h3 className=" h-5 flex rotate-90">DESARROLLADOR</h3>
+          </div>
+
+        </div>
+      </div >
     </>
   )
 }
